a0f9353a224e4fd1ff84ec266e4289141d3fe729
Derek Naing <dereknaing01@gmail.com>
2024-01-15 01:05:52 +0000

    started work on register account

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,24 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+
+@Service
 public class AccountService {
+
+    public void register(Account account) {
+        return;
+    }
+
+    public Account getAccountByUsername(String username) {
+        return ;
+    }
+
+    
+
+
+
+
+
 }
